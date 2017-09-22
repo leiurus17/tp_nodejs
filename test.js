0d43a79e036b8f93c562093522a7554cc79add75
Marlon <martianworm17@gmail.com>
2017-09-22 23:35:20 +0800

    Play with console.log

--- test.js
+++ test.js
@@ -14,3 +14,11 @@ y = 40
 /* You can do operations or assignments outside and the just print the output */
 // I am starting to like Node.js
 console.log(x + y)
+
+// Reset x variable as I will try it on a loop below
+x = 0
+
+do {
+	x++;
+	console.log("x: " + x);
+} while (x < 5);
