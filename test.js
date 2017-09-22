b7bff8ce7a1693e159d6552f2d8cc43471c00ab6
Marlon <martianworm17@gmail.com>
2017-09-22 23:14:51 +0800

    Play with console.log

--- test.js
+++ test.js
@@ -4,3 +4,13 @@
 
 /* Hello, World! program in node.js */
 console.log("Hello, World!")
+
+/* You can do operations inside the console.log */
+console.log(1 + 2)
+
+x = 10
+y = 40
+
+/* You can do operations or assignments outside and the just print the output */
+// I am starting to like Node.js
+console.log(x + y)
