50cdd8c565cd6ad66305532482620a48cd69630b
Marlon <martianworm17@gmail.com>
2017-09-24 22:40:52 +0800

    Node.js - Event Loop

---
+++ eventloop/main.js
@@ -0,0 +1,29 @@
+/**
+ * http://usejsdoc.org/
+ */
+// Import events module
+var events = require('events');
+
+// Create an eventEmitter as follows
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+	console.log('connection successful.');
+	
+	// Fire the data_received event
+	eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+	console.log('data received successfully.');
+});
+
+// Fire the connection event
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
