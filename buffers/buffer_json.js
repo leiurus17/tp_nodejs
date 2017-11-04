19509faa75874fea56c86700b9364596f4def04f
Marlon <martianworm17@gmail.com>
2017-11-04 22:35:46 +0800

    Convert Buffer to JSON

---
+++ buffers/buffer_json.js
@@ -0,0 +1,14 @@
+/**
+ * http://usejsdoc.org/
+ */
+buf = new Buffer(26);
+
+for (var i = 0; i < 26; i++) {
+	buf[i] = i + 97;
+}
+
+console.log(buf.toJSON());
+
+var json = buf.toJSON(buf);
+
+console.log(json);
